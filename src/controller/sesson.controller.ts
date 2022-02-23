065215d226ae7f2632d4a61fe457751e497ad1f4
Nazim <nazik_111@mail.ru>
2022-02-23 23:29:34 +0600

    feat(user-registration): add session controller

--- src/controller/sesson.controller.ts
+++ src/controller/sesson.controller.ts
@@ -1,5 +1,12 @@
 import { Request, Response } from "express";
+import { createSession } from "../service/session.service";
+import { validatePassword } from "../service/user.service";
 
 export async function createUserSessionHandler(req: Request, res: Response) {
+  const user = await validatePassword(req.body)
 
+  if (!user) return res.status(401).send("Invalid email or password")
+
+  const session = createSession(user._id, req.get("user-agent") || "")
+  console.log(session)
 }
