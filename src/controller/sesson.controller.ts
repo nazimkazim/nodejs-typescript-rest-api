bda7c168e89254c730ffe069d65fa50db2b1a414
etun <etun@technodom.kz>
2022-04-02 13:57:41 +0600

    feat(user-registration): add jwt

--- src/controller/sesson.controller.ts
+++ src/controller/sesson.controller.ts
@@ -1,12 +1,24 @@
 import { Request, Response } from "express";
+import config from 'config'
 import { createSession } from "../service/session.service";
 import { validatePassword } from "../service/user.service";
+import { signJwt } from "../utils/jwt.utils";
 
 export async function createUserSessionHandler(req: Request, res: Response) {
   const user = await validatePassword(req.body)
 
   if (!user) return res.status(401).send("Invalid email or password")
 
-  const session = createSession(user._id, req.get("user-agent") || "")
-  console.log(session)
+  const session = await createSession(user._id, req.get("user-agent") || "")
+  
+  const accessToken = signJwt(
+    {...user, session: session._id}, {expiresIn:config.get('accessTokenTt')}
+  )
+  
+  const refreshToken = signJwt(
+    {...user, session: session._id}, {expiresIn:config.get('refreshTokenTt')}
+  )
+
+  return res.send({accessToken, refreshToken})
+
 }
--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,16 @@
 import { Express, Request, Response } from "express";
+import { createUserSessionHandler } from "./controller/sesson.controller";
 import { createUserHandler } from "./controller/user.controller";
 import validateResource from "./middleware/validateResource";
+import { createSessionSchema } from "./schema/session.schema";
 import { createUserSchema } from "./schema/user.schema";
 
 function routes(app: Express) {
   app.get("/healthcheck", (req: Request, res: Response) => res.sendStatus(200));
 
   app.post("/api/users", validateResource(createUserSchema), createUserHandler);
+
+  app.post("/api/session", validateResource(createSessionSchema), createUserSessionHandler)
 }
 
 export default routes;
